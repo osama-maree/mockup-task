36954518808e87af2975db7b926186f52b059508
osama maree <osamamarei2001@gmail.com>
2023-08-31 15:22:44 +0300

    Add Architecture Design For Project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import "./App.css";
-
+function Mockup({ mockupData }) {
+  return (
+    <>
+      <h3>Add members to Front-end development team</h3>
+      <div>Find members</div>
+      <MockupBox mockupData={mockupData} />
+    </>
+  );
+}
+function MockupBox({ mockupData }) {
+  return <>moukup box</>;
+}
+function MyButton({ background, color }) {
+  return <>myButton</>;
+}
 
 const mockupData = [
   { name: "Corolien Bloeme", clicked: false },
   { name: "Sun Jun", clicked: true },
-  { name: "Song Bao", clicked: false},
+  { name: "Song Bao", clicked: false },
   { name: "Olivia Arribas", clicked: true },
   { name: "Bonginkosi Mdladlana", clicked: false },
-  { name:"Arina Belomestnykh", clicked: true },
+  { name: "Arina Belomestnykh", clicked: true },
   { name: "Jacqueline Likoki", clicked: true },
 ];
 function App() {
-  return <div></div>;
+  return <div>
+    <Mockup mockupData={mockupData}/>
+  </div>;
 }
 
 export default App;
