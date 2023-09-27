1bd3b43b246eb5e0689e23856b4ec25ee8ae6732
osama maree <osamamarei2001@gmail.com>
2023-09-27 15:50:44 +0300

    Add Mockup Data

---
+++ src/mockupData/mockData.js
@@ -0,0 +1,9 @@
+export const mockupData = [
+  { name: "Corolien Bloeme", clicked: false },
+  { name: "Sun Jun", clicked: true },
+  { name: "Song Bao", clicked: false },
+  { name: "Olivia Arribas", clicked: true },
+  { name: "Bonginkosi Mdladlana", clicked: false },
+  { name: "Arina Belomestnykh", clicked: true },
+  { name: "Jacqueline Likoki", clicked: true },
+];
